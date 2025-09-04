23c61cc4da1b9dd4698f2f49b27c6762e8b5618a
githubcq <1284518181@qq.com>
2025-09-04 17:58:43 +0800

    fix: vmalert parse nhi log response

--- app/vmalert/datasource/client_nhi_log.go
+++ app/vmalert/datasource/client_nhi_log.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -11,8 +12,14 @@ type nhiLogResponse struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
 	Data    struct {
-		Metrics []Metric `json:"metrics"`
+		Metrics []logMetric `json:"metrics"`
 	} `json:"data"`
+}
+
+type logMetric struct {
+	AggregationTs   []string          `json:"timestamp"`
+	AggregationInfo map[string]string `json:"labels"`
+	Values          []string          `json:"values"`
 }
 
 func parseNhiLogResponse(req *http.Request, resp *http.Response) (Result, error) {
@@ -20,7 +27,29 @@ func parseNhiLogResponse(req *http.Request, resp *http.Response) (Result, error)
 	if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
 		return Result{}, fmt.Errorf("error parsing sql metrics for %s: %w", req.URL.Redacted(), err)
 	}
-	return Result{Data: r.Data.Metrics}, nil
+	var metrics []Metric
+	for _, metric := range r.Data.Metrics {
+		var m Metric
+		for k, v := range metric.AggregationInfo {
+			m.AddLabel(k, v)
+		}
+		for _, value := range metric.Values {
+			valueFloat, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				return Result{}, fmt.Errorf("error parsing value %s: %w", value, err)
+			}
+			m.Values = append(m.Values, valueFloat)
+		}
+		for _, timestamp := range metric.AggregationTs {
+			timestamp, err := time.Parse(time.DateTime, timestamp)
+			if err != nil {
+				return Result{}, fmt.Errorf("error parsing timestamp %s: %w", timestamp, err)
+			}
+			m.Timestamps = append(m.Timestamps, timestamp.UnixMilli())
+		}
+		metrics = append(metrics, m)
+	}
+	return Result{Data: metrics}, nil
 }
 
 func (c *Client) setNhiLogReqParams(r *http.Request, query string, timestamp time.Time) error {
@@ -29,10 +58,10 @@ func (c *Client) setNhiLogReqParams(r *http.Request, query string, timestamp tim
 	}
 	q := r.URL.Query()
 	if c.applyIntervalAsTimeFilter && c.evaluationInterval > 0 {
-		q.Set("StartTime", timestamp.Add(-c.evaluationInterval).Format(time.DateTime))
-		q.Set("EndTime", timestamp.Format(time.DateTime))
+		q.Set("StartTime", timestamp.Add(-c.evaluationInterval).Local().Format(time.DateTime))
+		q.Set("EndTime", timestamp.Local().Format(time.DateTime))
 	} else {
-		q.Set("Time", timestamp.Format(time.DateTime))
+		q.Set("Time", timestamp.Local().Format(time.DateTime))
 	}
 	r.URL.RawQuery = q.Encode()
 	return c.setFormDataParams(r, query)
@@ -43,8 +72,8 @@ func (c *Client) setNhiLogRangeReqParams(r *http.Request, query string, start, e
 		r.URL.Path += "/backends/api/v1/log/stats_query_range"
 	}
 	q := r.URL.Query()
-	q.Add("StartTime", start.Format(time.DateTime))
-	q.Add("EndTime", end.Format(time.DateTime))
+	q.Add("StartTime", start.Local().Format(time.DateTime))
+	q.Add("EndTime", end.Local().Format(time.DateTime))
 	r.URL.RawQuery = q.Encode()
 	return c.setFormDataParams(r, query)
 }
